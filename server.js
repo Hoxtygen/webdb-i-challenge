ce2086aab71e3a72d4d234560b4a0b7a8dafc49f
Hoxtygen <hoxtygen@live.com>
2019-07-15 13:36:31 +0100

    Get all accounts

--- server.js
+++ server.js
@@ -5,5 +5,29 @@ const db = require('./data/dbConfig.js');
 const server = express();
 
 server.use(express.json());
+server.use(express.urlencoded({ extended: false }))
+
+function getAllAccounts() {
+    return db('accounts');
+  }
+
+  
+
+
+
+
+
+
+
+
+
+
+  // API endpoints
+  server.get('/accounts', async(req, res) => {
+      const accounts = await getAllAccounts();
+      return res.json(accounts);
+  });
+
+ 
 
 module.exports = server;
